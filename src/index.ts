60ef572a8df959f1308a4b2de925aaae6cda0e69
Lyor Besnainou <lior@tessan.io>
2020-02-21 11:36:05 +0100

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import socketServer from "socket.io";
+import { Socket } from "socket.io";
 import { Store } from "./store";
 import { ConferenceService } from "./conferenceService";
 import { middlware as errorMiddlware } from "./errors/errorMiddlware";
 import { ParticipantController } from "./controllers/SocketController";
+import express from "express";
 
-const io = socketServer(3000);
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 export const store = new Store();
 export const conferenceService = new ConferenceService();
 
-io.on("connection", socket => {
+io.on("connection", (socket: Socket) => {
   console.log(`new connection connection socket id: ${socket.id}`);
-
   socket.use(errorMiddlware(socket));
   new ParticipantController(socket);
 });
+
+server.listen(80);
