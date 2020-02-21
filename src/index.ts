14020251d3ce2296bd2346e4b72d9ff7f628b03a
Lyor Besnainou <lior@tessan.io>
2020-02-21 12:32:51 +0100

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,20 @@
-import { Socket } from "socket.io";
 import { Store } from "./store";
 import { ConferenceService } from "./conferenceService";
 import { middlware as errorMiddlware } from "./errors/errorMiddlware";
 import { ParticipantController } from "./controllers/SocketController";
 import express from "express";
+import socketIO from "socket.io";
 
-const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const app = express().listen(process.env.PORT, () =>
+  console.log("Listening on port: " + process.env.PORT)
+);
+const io = socketIO(app);
 io.origins("*:*");
 export const store = new Store();
 export const conferenceService = new ConferenceService();
 
-app.get("/", (req: any, res: any) => {
-  res.send({ status: "OK" });
-});
-
-io.on("connection", (socket: Socket) => {
+io.on("connection", (socket: socketIO.Socket) => {
   console.log(`new connection connection socket id: ${socket.id}`);
   socket.use(errorMiddlware(socket));
   new ParticipantController(socket);
-});
-
-io.listen(process.env.PORT || 5000, () => {
-  console.log("listening...");
 });
