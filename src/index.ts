d04192e832fdbfd0f6bd28aff8f7b3d0b9bdb000
Lyor Besnainou <lior@tessan.io>
2020-02-21 11:57:27 +0100

    add preinstall

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,6 @@ io.on("connection", (socket: Socket) => {
   new ParticipantController(socket);
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("listening...");
 });
