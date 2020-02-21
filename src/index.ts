ce697fe841e16011c1e1b3b8a2b5cde2883398b8
gwendal lasson <gwendal.lasson@icloud.com>
2020-02-21 11:08:22 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { middlware as errorMiddlware } from "./errors/errorMiddlware";
 import { ParticipantController } from "./controllers/SocketController";
 
 const io = socketServer(3000);
-io.origins("*:*");
+io.origins("localhost:4200");
 export const store = new Store();
 export const conferenceService = new ConferenceService();
 
